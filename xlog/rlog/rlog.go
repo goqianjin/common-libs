422cc1cf4e735dca645abb03fbcb2b0695674dec
Shen <shenqianjin@qiniu.com>
2024-09-08 15:53:52 +0800

    updated

--- xlog/rlog/rlog.go
+++ xlog/rlog/rlog.go
@@ -6,17 +6,26 @@ import (
 	"io"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/goqianjin/common-libs/xlog/internal"
 	"github.com/goqianjin/common-libs/xlog/internal/buffer"
+)
+
+const (
+	FieldNameMessage   = "MSG"
+	FieldNameArguments = "ARGS..."
+
+	TimeFormat = "2006-01-02 15:04:05.000000"
 )
 
 type rawLogger struct {
 	mu *sync.Mutex
 	w  io.Writer
 
-	level     internal.Level
-	separator string
+	level      internal.Level
+	separator  string
+	fieldNames []string
 }
 
 func (l *rawLogger) Log(ctx context.Context, level internal.Level, msg string, args ...any) {
@@ -27,20 +36,43 @@ func (l *rawLogger) Log(ctx context.Context, level internal.Level, msg string, a
 	defer buf.Free()
 
 	attrs := internal.GetContextAttributes(ctx)
-	for _, v := range attrs {
-		_, _ = buf.WriteString(fmt.Sprint(v))
-		_, _ = buf.WriteString(l.separator)
-	}
 
-	if len(args) > 0 {
+	if len(l.fieldNames) > 0 {
+		for _, name := range l.fieldNames {
+			switch name {
+			case FieldNameMessage:
+				_, _ = buf.WriteString(msg)
+			case FieldNameArguments:
+				for _, arg := range args {
+					appendTextValue(buf, arg)
+					_, _ = buf.WriteString(l.separator)
+				}
+			default:
+				appendTextValue(buf, attrs[name])
+				_, _ = buf.WriteString(l.separator)
+			}
+		}
+	} else {
+		// attributes
+		for k, v := range attrs {
+			_, _ = buf.WriteString(k)
+			_ = buf.WriteByte('=')
+			appendTextValue(buf, v)
+			_, _ = buf.WriteString(l.separator)
+		}
+		// message
+		//_, _ = buf.WriteString(msg)
+		appendTextValue(buf, msg)
+		_, _ = buf.WriteString(l.separator)
+		// arguments
 		for _, arg := range args {
 			appendTextValue(buf, arg)
 			_, _ = buf.WriteString(l.separator)
 		}
 	}
 
-	_, _ = buf.WriteString(msg)
-	if msg[len(msg)-1] != '\n' {
+	// try append '\n' in the end
+	if (*buf)[len(*buf)-1] != '\n' {
 		_ = buf.WriteByte('\n')
 	}
 
@@ -51,15 +83,17 @@ func (l *rawLogger) Log(ctx context.Context, level internal.Level, msg string, a
 	return
 }
 
-type c struct {
-}
-
-func (l *c) String() string {
-	return ""
-}
+const (
+	nilValue = '-'
+)
 
 func appendTextValue(buf *buffer.Buffer, v any) {
+	if v == nil {
+		v = nilValue
+	}
 	switch v.(type) {
+	case time.Time:
+		buf.WriteString(strconv.Quote(v.(time.Time).Format(TimeFormat)))
 	case string, fmt.Stringer:
 		buf.WriteString(strconv.Quote(fmt.Sprint(v)))
 	default:
--- xlog/rlog/rlow_test.go
+++ xlog/rlog/rlow_test.go
@@ -3,17 +3,37 @@ package rlog
 import (
 	"context"
 	"os"
+	"os/user"
 	"testing"
+	"time"
 
 	"github.com/goqianjin/common-libs/xlog/internal"
 )
 
 func TestRLog(t *testing.T) {
 	log := New(os.Stdout, Option{})
-	log.Log(context.Background(), internal.LevelTrace, "raw log trace info", "arg1", "arg2")
-	log.Log(context.Background(), internal.LevelDebug, "raw log debug info", "arg1", "arg2")
-	log.Log(context.Background(), internal.LevelInfo, "raw log info info", "arg1", "arg2")
-	log.Log(context.Background(), internal.LevelWarn, "raw log warn info", "arg1", "arg2")
-	log.Log(context.Background(), internal.LevelError, "raw log error info", "arg1", "arg2")
-	log.Log(context.Background(), internal.LevelFatal, "raw log fatal info", "arg1", "arg2")
+
+	// logging - basic
+	log.Log(context.Background(), internal.LevelTrace, "basic RLog by TRACE info")
+	log.Log(context.Background(), internal.LevelDebug, "basic RLog by DEBUG info")
+	log.Log(context.Background(), internal.LevelInfo, "basic RLog by INFO info")
+	log.Log(context.Background(), internal.LevelWarn, "basic RLog by WARN info")
+	log.Log(context.Background(), internal.LevelError, "basic RLog by ERROR info")
+	log.Log(context.Background(), internal.LevelFatal, "basic RLog by FATAL info")
+
+	// logging with arguments
+	log.Log(context.Background(), internal.LevelTrace, "RLog with args by TRACE info", "arg1", "arg2")
+	log.Log(context.Background(), internal.LevelDebug, "RLog with args by DEBUG info", "arg1", "arg2")
+	log.Log(context.Background(), internal.LevelInfo, "RLog with args by INFO info", "arg1", "arg2")
+	log.Log(context.Background(), internal.LevelWarn, "RLog with args by WARN info", "arg1", "arg2")
+	log.Log(context.Background(), internal.LevelError, "RLog with args by ERROR info", "arg1", "arg2")
+	log.Log(context.Background(), internal.LevelFatal, "RLog with args by FATAL info", "arg1", "arg2")
+
+	// logging - with attributes
+	ctx := internal.PutContextAttribute(context.Background(), "timeAttr1", time.Now())
+	ctx = internal.PutContextAttribute(ctx, "stringAttr1", "value of key1")
+	ctx = internal.PutContextAttribute(ctx, "intAttr1", 123)
+	ctx = internal.PutContextAttribute(ctx, "boolAttr1", true)
+	ctx = internal.PutContextAttribute(ctx, "objectAtt1", user.User{Uid: "10001", Username: "xlog owner"})
+	log.Log(ctx, internal.LevelInfo, "RLog with attributes and args by INFO info", "arg1", "arg2")
 }
